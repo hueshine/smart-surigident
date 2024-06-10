1101952d70a753ee77c4506092a3459478afdabb
neu-astha <kulchan.yogesh2@gmail.com>
2024-06-10 16:56:35 +0545

    product slide

--- js/main.js
+++ js/main.js
@@ -2,13 +2,30 @@ var bannerSlider = new Swiper(".hero", {
   loop: true,
   slidesPerView: 1,
   spaceBetween: 0,
-  speed: 1500,
-  effect: "fade",
+  speed: 1800,
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   autoplay: {
     delay: 6500,
+  },
+});
+
+var featuredSwiper = new Swiper(".featured_slider", {
+  loop: true,
+  slidesPerView: 3,
+  speed: 1500,
+  autoplay: {
+    delay: 4500,
+  },
+  grid: {
+    rows: 2,
+  },
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
